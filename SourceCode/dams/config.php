40b0f67d5bdda751cb49d4505a05a31bf0935849
MelvinIsHere <melvinfelicisimo@gmail.com>
2023-06-29 16:25:25 +0800

    update

--- SourceCode/dams/config.php
+++ SourceCode/dams/config.php
@@ -2,7 +2,7 @@
   $hostname = "localhost";
   $username = "root";
   $password = "";
-  $dbname = "technoartdb";
+  $dbname = "dams";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
